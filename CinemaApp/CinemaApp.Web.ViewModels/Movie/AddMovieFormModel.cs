3a5fcdda737a6d7b4d27615243f6261b9df58c98
TeodorIliev06 <tedo2505@gmail.com>
2025-01-16 00:15:14 +0200

    Implement movie display and add featureseatur

---
+++ CinemaApp/CinemaApp.Web.ViewModels/Movie/AddMovieFormModel.cs
@@ -0,0 +1,29 @@
+﻿namespace CinemaApp.Web.ViewModels.Movie
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Common.EntityValidationConstants.Movie;
+
+    public class AddMovieFormModel
+    {
+        [MaxLength(TitleMaxLength)]
+        public required string Title { get; set; } = null!;
+
+        [MinLength(GenreMinLength)]
+        [MaxLength(GenreMaxLength)]
+        public required string Genre { get; set; } = null!;
+
+        public required string ReleaseDate { get; set; } = null!;
+
+        [Range(DurationMinValue, DurationMaxValue)]
+        public int Duration { get; set; }
+
+        [MinLength(DirectorNameMinLength)]
+        [MaxLength(DirectorNameMaxLength)]
+        public required string Director { get; set; } = null!;
+
+        [MinLength(DescriptionMinLength)]
+        [MaxLength(DescriptionMaxLength)]
+        public required string Description { get; set; } = null!;
+    }
+}
---
+++ CinemaApp/CinemaApp.Web/Controllers/MovieController.cs
@@ -0,0 +1,89 @@
+﻿namespace CinemaApp.Web.Controllers
+{
+    using System.Globalization;
+    using CinemaApp.Web.ViewModels.Movie;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Data;
+    using Data.Models;
+
+    public class MovieController : Controller
+    {
+        private readonly CinemaDbContext dbContext;
+
+        public MovieController(CinemaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Movie> movies = this.dbContext
+                .Movies
+                .ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AddMovieFormModel model)
+        {
+            bool isReleaseDateValid = DateTime
+                .TryParseExact(model.ReleaseDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime releaseDate);
+
+            if (!isReleaseDateValid)
+            {
+                this.ModelState.AddModelError(nameof(model.ReleaseDate), "The release date must be in the following format: dd/MM/yyyy");
+            }
+
+            if (this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var movie = new Movie()
+            {
+                Title = model.Title,
+                Genre = model.Genre,
+                ReleaseDate = releaseDate,
+                Director = model.Director,
+                Duration = model.Duration,
+                Description = model.Description
+            };
+
+            this.dbContext.Movies.Add(movie);
+            this.dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(string id)
+        {
+            bool isGuidValid = Guid.TryParse(id, out Guid guid);
+
+            if (!isGuidValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var movie = this.dbContext
+                .Movies
+                .FirstOrDefault(m => m.Id == guid);
+
+            if (movie == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(movie);
+        }
+    }
+}
